fd608e0aa507fbd4b722169b0c47e6d740ee085c
M Reza Faisal <rezafaisal@users.noreply.github.com>
2023-03-06 08:49:31 +0800

    Add files via upload

---
+++ BAB_02/MyFirstJavaCode.java
@@ -0,0 +1,10 @@
+package Bab_02;
+
+/**
+ * MyFirstJavaCode
+ */
+public class MyFirstJavaCode {
+    public static void main(String[] args) {
+        System.out.println("Welcome Java in Visual Studio Code");
+    }
+}
